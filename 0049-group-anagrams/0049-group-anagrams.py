01b04296ca67fe1147612f44c1d785f104d8df88
Lucas Pavanelli <lucasapava@gmail.com>
2023-02-01 17:25:20 -0300

    Time: 157 ms (28.97%), Space: 17.2 MB (78.10%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,21 @@
 class Solution:
+    def countSort(self, s: str):
+        count_map = defaultdict(int)
+        alphabet = list(string.ascii_lowercase)
+        for c in s:
+            count_map[c] += 1
+        result = ''
+        for let in alphabet:
+            if let in count_map:
+                result += let * count_map[let]
+        return result
+        
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         result_map = defaultdict(list)
         for cur_str in strs:
             print(cur_str)
-            sorted_str = ''.join(sorted(cur_str))
+            # sorted_str = ''.join(sorted(cur_str))
+            sorted_str = self.countSort(cur_str)
             print(sorted_str)
             result_map[sorted_str].append(cur_str)    
         result = []
